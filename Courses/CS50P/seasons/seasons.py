b4c4c7b0d5dc873f68ae4786a4ea874c77d72702
Rijan-Joshi <csaijspy@gmail.com>
2024-06-08 23:48:13 +0545

    Solving seasons of love problems

---
+++ Courses/CS50P/seasons/seasons.py
@@ -0,0 +1,15 @@
+from datetime import date
+import sys
+import re
+
+def main():
+    input_date = input()
+    
+
+
+def check_format():
+    if (re.search(r"^(\d{4})-([0-9]|1[0-2])-([0-9]|[12][0-9]|3[01])$", input-date)) 
+
+
+if __name__ == "__main__":
+    main()
