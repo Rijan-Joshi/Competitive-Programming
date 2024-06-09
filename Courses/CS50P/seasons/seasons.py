835cee29ba71c85901c64e53a174dab1faad1660
Rijan-Joshi <csaijspy@gmail.com>
2024-06-09 09:52:56 +0545

    Completed the seasons of love.py main solution

--- Courses/CS50P/seasons/seasons.py
+++ Courses/CS50P/seasons/seasons.py
@@ -5,22 +5,24 @@ import inflect
 
 p = inflect.engine()
 
+
 def main():
     input_date = input("What's your birthdate? ")
     current_date = date.today()
     iDate = check_get_format(input_date, current_date)
-    total_minute = int((current_date - iDate).total_seconds() // 60)
-    print(p.number_to_words(total_minute, andword=",").title())
+    total_minute = (current_date - iDate).days * 24 * 60
+    print(current_date - iDate)
+    print(p.number_to_words(total_minute, andword=",").capitalize())
 
 
 def check_get_format(input_date, today):
-    if (re.search(r"^(\d{4})-(0[1-9]|1[0-2])-(0[0-9]|[12][0-9]|3[01])$", input_date)) :
+    if re.search(r"^(\d{4})-(0[1-9]|1[0-2])-(0[0-9]|[12][0-9]|3[01])$", input_date):
         try:
-            year, month, days =  map(int, input_date.split('-'))
+            year, month, days = map(int, input_date.split("-"))
             d = date(year, month, days)
             return d if today > d else False
         except ValueError:
-             return False
+            return False
     else:
         sys.exit("The date format is invalid.")
 
