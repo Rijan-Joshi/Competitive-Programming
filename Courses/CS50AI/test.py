67690384a91ca1f192d38d6639246ac96c305731
Rijan-Joshi <csaijspy@gmail.com>
2024-06-23 23:30:46 +0545

    Some additional helpful setups

--- Courses/CS50AI/test.py
+++ Courses/CS50AI/test.py
@@ -1,192 +1,50 @@
-class Sentence():
-    """
-    Logical statement about a Minesweeper game
-    A sentence consists of a set of board cells,
-    and a count of the number of those cells which are mines.
-    """
+#Setting Up PageRank Logic by myself 
 
-    def __init__(self, cells, count):
-        self.cells = set(cells)
-        self.count = count
+import os
+import random
+import re
+import sys
 
-    def __eq__(self, other):
-        return self.cells == other.cells and self.count == other.count
+DAMPING = 0.85
+SAMPLES = 10000
 
-    def __str__(self):
-        return f"{self.cells} = {self.count}"
-
-    def known_mines(self):
-        """
-        Returns the set of all cells in self.cells known to be mines.
-        """
-        if len(self.cells) == self.count:
-            return set(self.cells)
-        return set()
-
-    def known_safes(self):
-        """
-        Returns the set of all cells in self.cells known to be safe.
-        """
-        if self.count == 0:
-            return set(self.cells)
-        return set()
-
-    def mark_mine(self, cell):
-        """
-        Updates internal knowledge representation given the fact that
-        a cell is known to be a mine.
-        """
-        if cell in self.cells:
-            self.cells.remove(cell)
-            self.count -= 1
-
-    def mark_safe(self, cell):
-        """
-        Updates internal knowledge representation given the fact that
-        a cell is known to be safe.
-        """
-        if cell in self.cells:
-            self.cells.remove(cell)
+def main():
+    if (len(sys.argv) != 2):
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1])
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print("PageRank Results from Sampling (n={SAMPLES)")
+    for page in sorted(ranks):
+        print(f"    {page}: {ranks[page]:.4f}")
+    ranks = iterate_pagerank(corpus, DAMPING)
+    for page in sorted(ranks):
+        print(f"   {page}: {ranks[page]:.4f}")
 
 
-class MinesweeperAI():
-    """
-    Minesweeper game player
-    """
+def crawl(directory):
 
-    def __init__(self, height=8, width=8):
+    pages = dict()
 
-        # Set initial height and width
-        self.height = height
-        self.width = width
+    for filename in os.listdir(directory):
+        if not filename.endswith(".html"):
+            continue
+        with open(os.path.join(directory, filename)) as f:
+            contents = f.read()
+            links = re.findall(r"<a\s+(?:[^>]*?)href=\"([^\"]*)\"", contents)
+            pages[filename] = set(links) - {filename}
 
-        # Keep track of which cells have been clicked on
-        self.moves_made = set()
+    #Exclude all external links out of the corpus
+    for filename in pages:
+        pages[filename] = set(
+            link for link in pages[filename]
+            if link in pages
+        )
 
-        # Keep track of cells known to be safe or mines
-        self.mines = set()
-        self.safes = set()
+def transition_model(corpus, page, damping_factor):
+    pass
 
-        # List of sentences about the game known to be true
-        self.knowledge = []
+def sample_pagerank():
+    ...
 
-    def mark_mine(self, cell):
-        """
-        Marks a cell as a mine, and updates all knowledge
-        to mark that cell as a mine as well.
-        """
-        self.mines.add(cell)
-        for sentence in self.knowledge:
-            sentence.mark_mine(cell)
-
-    def mark_safe(self, cell):
-        """
-        Marks a cell as safe, and updates all knowledge
-        to mark that cell as safe as well.
-        """
-        self.safes.add(cell)
-        for sentence in self.knowledge:
-            sentence.mark_safe(cell)
-
-    def add_knowledge(self, cell, count):
-        """
-        Called when the Minesweeper board tells us, for a given
-        safe cell, how many neighboring cells have mines in them.
-
-        This function should:
-            1) mark the cell as a move that has been made
-            2) mark the cell as safe
-            3) add a new sentence to the AI's knowledge base
-               based on the value of `cell` and `count`
-            4) mark any additional cells as safe or as mines
-               if it can be concluded based on the AI's knowledge base
-            5) add any new sentences to the AI's knowledge base
-               if they can be inferred from existing knowledge
-        """
-        # 1) mark the cell as a move that has been made
-        self.moves_made.add(cell)
-
-        # 2) mark the cell as safe
-        self.mark_safe(cell)
-
-        # 3) add a new sentence to the AI's knowledge base
-        neighbors = set()
-        for i in range(cell[0] - 1, cell[0] + 2):
-            for j in range(cell[1] - 1, cell[1] + 2):
-                if (i, j) == cell:
-                    continue
-                if 0 <= i < self.height and 0 <= j < self.width:
-                    if (i, j) not in self.safes and (i, j) not in self.mines:
-                        neighbors.add((i, j))
-        new_sentence = Sentence(neighbors, count)
-        self.knowledge.append(new_sentence)
-
-        # 4) mark any additional cells as safe or as mines
-        self.update_knowledge_base()
-
-        # 5) add any new sentences to the AI's knowledge base
-        new_inferences = []
-        for sentence1 in self.knowledge:
-            if not sentence1.cells:
-                continue
-            for sentence2 in self.knowledge:
-                if sentence1 == sentence2 or not sentence2.cells:
-                    continue
-                if sentence1.cells < sentence2.cells:
-                    inferred_cells = sentence2.cells - sentence1.cells
-                    inferred_count = sentence2.count - sentence1.count
-                    new_inference = Sentence(inferred_cells, inferred_count)
-                    if new_inference not in self.knowledge and new_inference not in new_inferences:
-                        new_inferences.append(new_inference)
-        self.knowledge.extend(new_inferences)
-
-    def update_knowledge_base(self):
-        """
-        Updates the knowledge base by marking cells as safe or as mines
-        if it can be concluded based on the AI's knowledge base.
-        """
-        all_safes = set()
-        all_mines = set()
-        for sentence in self.knowledge:
-            if sentence.known_safes():
-                all_safes.update(sentence.known_safes())
-            if sentence.known_mines():
-                all_mines.update(sentence.known_mines())
-        for safe in all_safes:
-            self.mark_safe(safe)
-        for mine in all_mines:
-            self.mark_mine(mine)
-
-        # Remove empty sentences
-        self.knowledge = [sentence for sentence in self.knowledge if sentence.cells]
-
-    def make_safe_move(self):
-        """
-        Returns a safe cell to choose on the Minesweeper board.
-        The move must be known to be safe, and not already a move
-        that has been made.
-
-        This function may use the knowledge in self.mines, self.safes
-        and self.moves_made, but should not modify any of those values.
-        """
-        for cell in self.safes:
-            if cell not in self.moves_made:
-                return cell
-        return None
-
-    def make_random_move(self):
-        """
-        Returns a move to make on the Minesweeper board.
-        Should choose randomly among cells that:
-            1) have not already been chosen, and
-            2) are not known to be mines
-        """
-        choices = [
-            (i, j)
-            for i in range(self.height)
-            for j in range(self.width)
-            if (i, j) not in self.moves_made and (i, j) not in self.mines
-        ]
-        if choices:
-            return random.choice(choices)
-        return None
+def iterate_pagerank():
+    pass
