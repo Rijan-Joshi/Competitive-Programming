94a23c6406d904b429a163abc513996368732e17
Rijan Shrestha <157405880+Rijan-Joshi@users.noreply.github.com>
2024-07-10 23:50:39 +0545

    Update zipped.py

--- Hackerrank/Python/zipped.py
+++ Hackerrank/Python/zipped.py
@@ -1,7 +1,7 @@
 #Using zipping in python for the first time
 
-N, Y = map(int, input().split())
-marks = [list(map(float, input().split())) for _ in range(Y)]
+N, y = map(int, input().split())
+marks = [list(map(float, input().split())) for _ in range(y)]
 
 for m in zip(*marks):
-    print(sum(m)/Y)
+    print(sum(m)/y)
